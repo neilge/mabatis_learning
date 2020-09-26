9d02c193a4f05d7b9f7fc2e8080db62f6d466c76
neilge001 <neilge001@gmail.com>
2020-09-26 14:32:51 -0700

    First case with annotation configuration.

--- src/main/java/com/neilge/dao/UserDao.java
+++ src/main/java/com/neilge/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.neilge.dao;
 
 import com.neilge.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ import java.util.List;
  * @since 09/26/2020-11:49 AM
  */
 public interface UserDao {
+
+    @Select("select * from user")
     List<User> findAll();
 }
